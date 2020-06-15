f58ae595d4779047f820eb6c4c1bb92f03a83ae3
John Mark Goodson <john.mark.hat@gmail.com>
2020-06-15 14:24:02 -0400

    added genre details page and added some comments on its functionality

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,7 @@
 var Genre = require('../models/genre');
+var Book = require('../models/book');
+var async = require('async');
+
 
 // Display list of all Authors.
 exports.genre_list = function(req, res, next) {
@@ -15,8 +18,33 @@ exports.genre_list = function(req, res, next) {
 };
 
 // Display detail page for a specific Genre.
-exports.genre_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
+exports.genre_detail = function(req, res, next) {
+
+    // use async parallel to to query for genre name and associated books.
+    async.parallel({
+        // query genre name
+        genre: function(callback) {
+            Genre.findById(req.params.id) // genre id is encoded at end of URL and fetched with params.id
+              .exec(callback);
+        },
+
+        // query books in genre
+        genre_books: function(callback) {
+            Book.find({ 'genre': req.params.id })
+              .exec(callback);
+        },
+
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.genre==null) { // No results.
+            var err = new Error('Genre not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render
+        res.render('genre_detail', { title: 'Genre Detail', genre: results.genre, genre_books: results.genre_books } );
+    });
+
 };
 
 // Display Genre create form on GET.
